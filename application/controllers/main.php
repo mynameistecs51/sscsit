ef240586b478e659009f8486ae24fd32e0666989
chaiwat <mynameistecs51@gmail.com>
2014-12-14 10:11:38 +0700

    chaiwat update data insert controll page send-paper

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -15,11 +15,25 @@ class Main extends CI_Controller {
 	}
 
 	public function add_project(){
+		$data_paper ="";
+
+		$select_paper = $this->input->post('select_paper');
+		foreach ($select_paper as $key_select_paper => $data_select) {
+			$data_paper .= $data_select.",";	//หาค่า select paper จาก array
+			}
+
 		$insert_paper = array(
-			'sex' => $this->input->post(),
-			'inputName1' => $this->input->post(),
+			'sex' => $this->input->post('sex'),
+			'inputName1' => $this->input->post('inputName1'),
+			'sex2' => $tihs->input->post('sex2'),
+			'inputName2' => $tihs->input->post('inputName2'),
+			'inputProjectName_TH' => $this->input->post("inputProjectName_TH"),
+			'inputProjectName_ENG' => $ths->input->post('inputProjectName_ENG'),
+			'select_paper' => substr($data_paper, 0,-1),
+			'fileProject' => $this->input->post('fileProject'),
+			'filePictureProject' => $this->input->post('filePictureProject'),
 		);
-		print_r($insert_paper);
+			
 	}
 
 	public function status_page(){
--- application/views/send-paper.php
+++ application/views/send-paper.php
@@ -25,135 +25,134 @@
                     <h4 class="glyphicon glyphicon-file">  โครงงาน</h4>
                 </div>
                 <div class="panel panel-body"> <!-- strat body panel -->
-                   <!--  <form class="form-horizontal col-sm-10 pull-left" role="form">   -->  <!--   start form -->
-                    <?php echo form_open_multipart('main/add_project','class="form-horizontal col-sm-10 pull-left" role="form"');?>
-                        <div class="form-group">
-                            <label for="inputName1" class="col-sm-3 control-label">ผู้ส่งโครงงาน 1</label>
-                            <div class="col-sm-7">
-                                <div class="input-group">
-                                    <div class="input-group-addon">
-                                        <label for="male">Male
-                                            <input type="radio"  value="male" name="sex" id="male"/>
-                                        </label>
-                                        <label for="female">Female
-                                            <input type="radio" value="female" name="sex" id="female">
-                                        </label>
-                                    </div>
-                                    <input type="text" class="form-control " name="inputName1" id="inputName1" placeholder="ชื่อสกุล  ผู้ส่งโครงงาน 1"  onfocus="" />
-                                </div>
+                 <!--  <form class="form-horizontal col-sm-10 pull-left" role="form">   -->  <!--   start form -->
+                 <?php echo form_open_multipart('main/add_project','class="form-horizontal col-sm-10 pull-left" role="form"');?>
+                 <div class="form-group">
+                    <label for="inputName1" class="col-sm-3 control-label">ผู้ส่งโครงงาน 1</label>
+                    <div class="col-sm-7">
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <label for="male">Male
+                                    <input type="radio"  value="male" name="sex" id="male"/>
+                                </label>
+                                <label for="female">Female
+                                    <input type="radio" value="female" name="sex" id="female">
+                                </label>
                             </div>
+                            <input type="text" class="form-control " name="inputName1" id="inputName1" placeholder="ชื่อสกุล  ผู้ส่งโครงงาน 1"  onfocus="" />
                         </div>
-                        <div class="form-group">
-                            <label for="inputName2" class="col-sm-3 control-label">ผู้ส่งโครงงาน 2</label>
-                            <div class="col-sm-7">
-                                <div class="input-group">
-                                    <div class="input-group-addon">
-                                        <label for="male2">Male
-                                            <input type="radio" value="male" name="sex2" id="male2"/>
-                                        </label>
-                                        <label for="female">Female
-                                            <input type="radio" value="female" name="sex2" id="female2">
-                                        </label>
-                                    </div>
-                                    <input type="text" class="form-control" name="inputName2" id="inputName2" placeholder="ชื่อสกุล  ผู้ส่งโครงงาน 2">
-                                </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputName2" class="col-sm-3 control-label">ผู้ส่งโครงงาน 2</label>
+                    <div class="col-sm-7">
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <label for="male2">Male
+                                    <input type="radio" value="male" name="sex2" id="male2"/>
+                                </label>
+                                <label for="female">Female
+                                    <input type="radio" value="female" name="sex2" id="female2">
+                                </label>
                             </div>
+                            <input type="text" class="form-control" name="inputName2" id="inputName2" placeholder="ชื่อสกุล  ผู้ส่งโครงงาน 2">
                         </div>
-                        <div class="form-group"> 
-                            <label for="inputProjectName_TH" class="col-sm-3 control-label">ชื่อโปรเจ็ค (ภาษาไทย)</label>
-                            <div class="col-sm-7">                           
-                                <input type="text" class="form-control" id="inputProjectName_TH" name="inputProjectName_TH" placeholder="ชื่อโปรเจ็คภาษาไทย">
-                            </div>
+                    </div>
+                </div>
+                <div class="form-group"> 
+                    <label for="inputProjectName_TH" class="col-sm-3 control-label">ชื่อโปรเจ็ค (ภาษาไทย)</label>
+                    <div class="col-sm-7">                           
+                        <input type="text" class="form-control" id="inputProjectName_TH" name="inputProjectName_TH" placeholder="ชื่อโปรเจ็คภาษาไทย">
+                    </div>
+                </div>
+                <div class="form-group"> 
+                    <label for="inputProjectName_ENG" class="col-sm-3 control-label">ชื่อโปรเจ็ค (ภาษาอังกฤษ)</label>
+                    <div class="col-sm-7">                           
+                        <input type="text" class="form-control" id="inputProjectName_ENG" name="inputProjectName_ENG" placeholder="ชื่อโปรเจ็คภาษาอังกฤษ">
+                    </div>
+                </div>
+                <div class="form-group"> 
+                    <label for="inputGroup" class="col-sm-3 control-label">ประเภทโครงงาน</label>
+                    <div class="col-sm-7">                           
+                    <select  class="form-control "  id="select_paper" name="select_paper[]" multiple>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputFileProject" class="col-sm-3 control-label">ไฟล์โปรเจ็ค</label>
+                    <div class="col-sm-7">
+                        <div class="input-group">
+                            <input type="file" name="fileProject" id="fileProject" />
+                            <p class="help-block">สามารถอัพโหลดไฟล์ .doc .docx .pdf <br/>*ควรตั้งชื่อเป็นภาษาอังกฤษ</p>                                               
                         </div>
-                        <div class="form-group"> 
-                            <label for="inputProjectName_ENG" class="col-sm-3 control-label">ชื่อโปรเจ็ค (ภาษาอังกฤษ)</label>
-                            <div class="col-sm-7">                           
-                                <input type="text" class="form-control" id="inputProjectName_ENG" name="inputProjectName_ENG" placeholder="ชื่อโปรเจ็คภาษาอังกฤษ">
-                            </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPictureProject" class="col-sm-3 control-label">รูปภาพโปรเจ็ค</label>
+                    <div class="col-sm-7">
+                        <div class="input-group">
+                            <input type="file" name="filePictureProject" id="filePicture" />
+                            <p class="help-block">สามารถอัพโหลดไฟล์ .png .jpg .JPG<br/>*ควรตั้งชื่อเป็นภาษาอังกฤษ</p>                                            
                         </div>
-                        <div class="form-group"> 
-                            <label for="inputGroup" class="col-sm-3 control-label">ประเภทโครงงาน</label>
-                            <div class="col-sm-7">                           
-                                <select  class="form-control" multiple id="select_paper">
-                                    <option>---- ประเภทโครงงาน ----</option>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputFileProject" class="col-sm-3 control-label">ไฟล์โปรเจ็ค</label>
-                            <div class="col-sm-7">
-                                <div class="input-group">
-                                    <input type="file" name="fileProject" id="fileProject" />
-                                    <p class="help-block">สามารถอัพโหลดไฟล์ .doc .docx .pdf <br/>*ควรตั้งชื่อเป็นภาษาอังกฤษ</p>                                               
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPictureProject" class="col-sm-3 control-label">รูปภาพโปรเจ็ค</label>
-                            <div class="col-sm-7">
-                                <div class="input-group">
-                                    <input type="file" name="filePictureProject" id="filePicture" />
-                                    <p class="help-block">สามารถอัพโหลดไฟล์ .png .jpg .JPG<br/>*ควรตั้งชื่อเป็นภาษาอังกฤษ</p>                                            
-                                </div>
-                            </div>
-                        </div>                    
-                        <div class="form-group pull-right">
-                            <!-- <div class="col-sm-offset-2 col-sm-10"> -->
-                            <button type="reset" class="btn btn-warning">Cancel </button>
-                            <button type="submit" class="btn btn-success">Send  </button>
-                            <!-- </div> -->
-                        </div>
-                    </form>  <!-- end form -->
-                </div>  <!-- end panel body -->
-            </div> <!-- end panel -->
-        </div>
-    </div><!--/.row-->
+                    </div>
+                </div>                    
+                <div class="form-group pull-right">
+                    <!-- <div class="col-sm-offset-2 col-sm-10"> -->
+                    <button type="reset" class="btn btn-warning">Cancel </button>
+                    <button type="submit" class="btn btn-success">Send  </button>
+                    <!-- </div> -->
+                </div>
+            </form>  <!-- end form -->
+        </div>  <!-- end panel body -->
+    </div> <!-- end panel -->
+</div>
+</div><!--/.row-->
 
-    <div class="gap"></div>
-    <h1 class="center">Meet the Team</h1>
-    <p class="lead center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-    <div class="gap"></div>
+<div class="gap"></div>
+<h1 class="center">Meet the Team</h1>
+<p class="lead center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+<div class="gap"></div>
 
-    <div id="meet-the-team" class="row">
-        <div class="col-md-3 col-xs-6">
-            <div class="center">
-                <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
-            </div>
+<div id="meet-the-team" class="row">
+    <div class="col-md-3 col-xs-6">
+        <div class="center">
+            <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
+            <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+            <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
         </div>
+    </div>
 
-        <div class="col-md-3 col-xs-6">
-            <div class="center">
-                <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
-            </div>
-        </div>        
-        <div class="col-md-3 col-xs-6">
-            <div class="center">
-                <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
-            </div>
-        </div>        
-        <div class="col-md-3 col-xs-6">
-            <div class="center">
-                <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
-                <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
-                <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
-                <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
-            </div>
+    <div class="col-md-3 col-xs-6">
+        <div class="center">
+            <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
+            <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+            <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
         </div>
-    </div><!--/#meet-the-team-->
+    </div>        
+    <div class="col-md-3 col-xs-6">
+        <div class="center">
+            <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
+            <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+            <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
+        </div>
+    </div>        
+    <div class="col-md-3 col-xs-6">
+        <div class="center">
+            <p><img class="img-responsive img-thumbnail img-circle" src="images/team-member.jpg" alt="" ></p>
+            <h5>David J. Robbins<small class="designation muted">Senior Vice President</small></h5>
+            <p>Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor.</p>
+            <a class="btn btn-social btn-facebook" href="#"><i class="icon-facebook"></i></a> <a class="btn btn-social btn-google-plus" href="#"><i class="icon-google-plus"></i></a> <a class="btn btn-social btn-twitter" href="#"><i class="icon-twitter"></i></a> <a class="btn btn-social btn-linkedin" href="#"><i class="icon-linkedin"></i></a>
+        </div>
+    </div>
+</div><!--/#meet-the-team-->
 </section><!--/#about-us-->
 
 <section id="bottom" class="wet-asphalt">
