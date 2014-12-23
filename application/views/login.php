1512694f7af61592a679090be7469737a105410a
chaiwat <mynameistecs51@gmail.com>
2014-12-23 22:06:25 +0700

    chaiwat update function login modal

--- application/views/login.php
+++ application/views/login.php
@@ -5,23 +5,23 @@
 	});
 </script>
 <div class="row col-sm-12">
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-	mymodal
-</button>
-<input type="text" class="form-control col-sm-6" placeholder="Text input">
-<!-- <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" > -->
-<div id="myModal"  class="modal fade bs-example-modal-lg" tabindex="1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" >
-	<!-- <div class="modal-dialog " style="width:60%" > -->
-	<div class="modal-dialog modal-lg ">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-				<h4 class="modal-title">Login Detail</h4>
-				<hr/>
-			</div>
-			<div class="modal-body">
-				
-				<?php 
+	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+		mymodal
+	</button>
+	<input type="text" class="form-control col-sm-6" placeholder="Text input">
+	<!-- <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" > -->
+	<div id="myModal"  class="modal fade bs-example-modal-lg" tabindex="1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" >
+		<!-- <div class="modal-dialog " style="width:60%" > -->
+		<div class="modal-dialog modal-lg ">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title">Login Detail</h4>
+					<hr/>
+				</div>
+				<div class="modal-body">
+
+					<?php 
 				// if(!$fb_data['me']){     
 				// 	echo anchor($fb_data['loginUrl'],'<button >login</button>');
 				// 	echo "<br/>";
@@ -35,49 +35,49 @@
 				// 	print_r($fb_data);
 
 				// } 
-				?>
+					?>
 
-				<form class="form-horizontal col-sm-12" role="form">
-					<div class="form-group">
-					<label for="inputEmail3" class="col-sm-2 control-label">FB ID</label>
-						<div class="col-sm-8">
-							<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+					<form class="form-horizontal col-sm-16" role="form">
+						<div class="form-group ">
+							<label for="inputfb_id" class="col-sm-2 control-label">FB ID</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" id="inputfb_id" placeholder="Email">
+							</div>
+							<label for="inputPassword3" class="col-sm-2 control-label">FB name</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" placeholder="Text input" width="120px;">
+							</div>						
 						</div>
-					</div>
-					<div class="form-group ">
-						<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-						<div class="col-sm-8">
-							<input type="text" class="form-control" placeholder="Text input" width="120px;">
-						</div>
-					</div>
-					<div class="form-group col-sm-16">
-						<label for="inputText" class="col-sm-2 control-label">Test</label>
-						<div class="col-sm-10">
-							<input type="text" class="form-control" id="inputText" placeholder="Text input">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-sm-offset-2 col-sm-10">
-							<div class="checkbox">
-								<label>
-									<input type="checkbox"> Remember me
-								</label>
+						<div class="form-group ">
+							<label for="inputPassword3" class="col-sm-2 control-label">Email</label>
+							<div class="col-sm-8">
+								<input type="email" class="form-control" placeholder="Text input" width="120px;">
 							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-sm-offset-2 col-sm-10">
-							<button type="submit" class="btn btn-default">Sign in</button>
+						<hr/>
+						<div class="form-group col-sm-16 ">
+							<label for="inputText" class="col-sm-3 control-label">First Name</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" id="inputText" placeholder="Text input">
+							</div>
+							<label for="inputText" class="col-sm-3 control-label">Last Name</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" id="inputText" placeholder="Text input">
+							</div>
 						</div>
-					</div>
-				</form>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-primary pull-left" data-dismiss="modal">FB Login</button>
-				<button type="button" class="btn btn-success">Account Login</button>
-			</div>
-		</div><!-- /.modal-content -->
-	</div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<button type="submit" class="btn btn-default">Sign in</button>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-primary pull-left" data-dismiss="modal">FB Login</button>
+					<button type="button" class="btn btn-success">Account Login</button>
+				</div>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
 </div>
 <?php $this->load->view('footer');?>
