94c5e7cd41dd9a2fe0a4e961c00499af2340b860
chaiwat <mynameistecs51@gmail.com>
2015-03-12 11:43:34 +0700

    chaiwat update form login

--- application/views/login.php
+++ application/views/login.php
@@ -27,62 +27,98 @@
 					<input type="hidden" name="inputEmail" value="<?php echo $fb_data['me']['email'];?>"> 
 					<input type="hidden" name="inputGender" value="<?php echo $fb_data['me']['gender'];?>">
 
-						<div class="form-group">
-							<div class="col-md-12">
-								<div class="form-group row">
-									<label for="inputFB_ID" class="col-md-2 control-label">FB ID</label>
-									<div class="col-md-3">
-										<input type="text" class="form-control " id="inputFB_ID" name="inputFB_ID" value="<?php echo $fb_data['me']['id'];?>" disabled="true">
+					<div class="form-group">
+						<div class="col-md-12">
+							<div class="form-group row">
+								<label for="inputFB_ID" class="col-md-2 control-label">FB ID</label>
+								<div class="col-md-3">
+									<input type="text" class="form-control " id="inputFB_ID" name="inputFB_ID" value="<?php echo $fb_data['me']['id'];?>" disabled="true">
+								</div>
+								<label for="inputFB_name" class="col-md-2 control-label">FB NAME</label>
+								<div class="col-md-3">
+									<input type="text" class="form-control" id="inputFB_name" name="inputFB_name" value="<?php echo $fb_data['me']['name'];?>"  disabled="true">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="form-group center">
+						<div class="col-md-14">
+							<div class="form-group row">
+								<label for="inputEmail" class="col-md-2 control-label">Email</label>
+								<div class="col-md-5">
+									<input type="email" class="form-control" id="inputEmail" name="inputEmail"value="<?php echo $fb_data['me']['email'];?>"  disabled="true">
+								</div>
+								<label for="inputGender" class="col-md-1 control-label">Gender</label>
+								<div class="col-md-3">
+									<input type="email" class="form-control" id="inputGender" name="inputGender"value="<?php echo $fb_data['me']['gender'];?>"  disabled="true">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-md-10 ">
+							<div class="form-group row">
+								<label for="inputEmail" class="col-md-3 control-label">Login Detail</label>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-md-10">
+							<div class="form-group row">
+								<label for="inputFirst_name" class="col-md-3 control-label">First Name</label>
+								<div class="col-md-3">
+									<input type="text" class="form-control " id="inputFirst_name" name="inputFirst_name" placeholder="inputFirst_name">
+								</div>
+								<label for="inputLast_name" class="col-md-3 control-label">Last name</label>
+								<div class="col-md-3">
+									<input type="text" class="form-control" id="inputLast_name" name="inputLast_name" placeholder="Value">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class  ="form-group">
+						<div class  ="col-md-10">
+							<div class="form-group row">
+								<label for="status_user" class="col-md-4 control-label">สถานะผู้เข้าร่วม</label>
+
+								<div class  ="input-group">
+									<div class="checkbox">
+										<label for  ="student_udru">
+											<input type ="radio"  value="student_udru" name="status_user" id="student_udru" checked="true" />  นักศึกษาภายในผู้ส่งผลงาน
+										</label>
 									</div>
-									<label for="inputFB_name" class="col-md-2 control-label">FB NAME</label>
-									<div class="col-md-3">
-										<input type="text" class="form-control" id="inputFB_name" name="inputFB_name" value="<?php echo $fb_data['me']['name'];?>"  disabled="true">
+									<div class="checkbox">
+										<label for  ="student_paper">
+											<input type ="radio" value="student_paper" name="status_user" id="student_paper">  นักศึกษาภายนอกผู้ส่งผลงาน
+										</label>
+									</div>
+									<div class="checkbox">
+										<label for  ="student_public">
+											<input type ="radio" value="student_public" name="status_user" id="student_public">  นักศึกษาภายนอกผู้เข้าร่วมงาน
+										</label>
+									</div>
+									<div class="checkbox">
+										<label for  ="teacher_public">
+											<input type ="radio" value="teacher_public" name="status_user" id="teacher_public">  อาจารย์ภายนอก
+										</label>
+									</div>
+									<div class="checkbox">
+										<label for  ="people_public">
+										<input type ="radio" value="people_public" name="status_user" id="people_public">  บุคคลทั่วไป
+										</label>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div class="form-group center">
-							<div class="col-md-14">
-								<div class="form-group row">
-									<label for="inputEmail" class="col-md-2 control-label">Email</label>
-									<div class="col-md-5">
-										<input type="email" class="form-control" id="inputEmail" name="inputEmail"value="<?php echo $fb_data['me']['email'];?>"  disabled="true">
-									</div>
-									<label for="inputGender" class="col-md-1 control-label">Gender</label>
-									<div class="col-md-3">
-										<input type="email" class="form-control" id="inputGender" name="inputGender"value="<?php echo $fb_data['me']['gender'];?>"  disabled="true">
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="form-group">
-							<div class="col-md-10 ">
-								<div class="form-group row">
-									<label for="inputEmail" class="col-md-3 control-label">Login Detail</label>
-								</div>
-							</div>
-						</div>
-						<div class="form-group">
-							<div class="col-md-10">
-								<div class="form-group row">
-									<label for="inputFirst_name" class="col-md-3 control-label">First Name</label>
-									<div class="col-md-3">
-										<input type="text" class="form-control " id="inputFirst_name" name="inputFirst_name" placeholder="inputFirst_name">
-									</div>
-									<label for="inputLast_name" class="col-md-3 control-label">Last name</label>
-									<div class="col-md-3">
-										<input type="text" class="form-control" id="inputLast_name" name="inputLast_name" placeholder="Value">
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="modal-footer">
-							<button type="submit" class="btn btn-success">Account Login</button>
-						</div>
-					</form>
+					</div>
 				</div>
-			</div><!-- /.modal-content -->
-		</div><!-- /.modal-dialog -->
-	</div><!-- /.modal -->
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-success">Account Login</button>
+				</div>
+			</form>
+		</div>
+	</div><!-- /.modal-content -->
+</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 </div>
 <?php $this->load->view('footer');?>
