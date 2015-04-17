1a9b8bd55740006d4df67c30b8e40f8d071089b9
Chaiwat <mynameistecs51@gmail.com>
2015-04-17 11:56:45 +0700

    chaiwat update controller/verifyLogin

--- application/controllers/verifyLogin.php
+++ application/controllers/verifyLogin.php
@@ -13,7 +13,36 @@ class VerifyLogin extends CI_Controller {
 
 	function index(){
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('useremail','Useremail','trim(')
+		$this->form_validation->set_rules('useremail','Useremail','trim|required|xss_clean');
+		$this->form_validation->set_rules('userpassword','Userpassword','trim|required|xss_clean|callback_check_database');
+
+		if($this->form_validation->run() === FALSE){
+			redirect('main/login','refresh');
+		}else{
+			redirect('main','refresh');
+		}
+	}
+
+	function check_database(){
+		//field input email
+		$useremail = $this->input->post('useremail');
+
+		$result = $this->user->login($useremail,$userpassword);
+
+		if($result){
+			$sess_array();
+			foreach ($result as $row_result) {
+					$sess_array = array(
+						'id' =>$row->id,
+						'username' => $row->username,
+					);
+					$this->session->set_userdata('fb_data',$sess_array);
+			}
+			return TRUE;
+		}else{
+			$this->form_validation->set_message('check_database','Invalid useremail && userpassword');
+			return FALSE;
+		}
 	}
 }
 ?>
